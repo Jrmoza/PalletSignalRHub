14900f1bea68de063907be60286bc2e619d77a5e
Jraza <jeanramos455@gmail.com>
2025-10-18 22:05:31 -0500

    VERSION PARA MEJORA MODULOS

--- Hubs/PalletHub.cs
+++ Hubs/PalletHub.cs
@@ -309,7 +309,7 @@ namespace PalletSignalRHub.Hubs
                 _logger.LogWarning("⚠️ Dispositivo no encontrado para envío de embalajes bicolor: {DeviceId}", deviceId);
             }
         }
-        // Agregar en Hubs/PalletHub.cs  VERSION PARA MEJORA MODULOS POR FAVOR
+        // Agregar en Hubs/PalletHub.cs  VERSIO DE VERSION PARA MEJORA MODULOS
         public async Task SendPalletInfoToMobile(string tripId, string infoMessage, string deviceId)
         {
             if (_deviceConnections.TryGetValue(deviceId, out string? connectionId))
